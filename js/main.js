ca24d3c87e8ef8d382639599297910922e0cb765
OblomovA <nastya.oblomskaya@gmail.com>
2019-07-18 09:15:44 +0300

    images for swiper added, js code complemented

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
         $('.nav-mobile ul').toggleClass('is-open')
     })
 })
+
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var mySwiper = window.matchMedia("(min-width: 640px)")
+    new Swiper('.swiper-container', {
+        autoplay: {
+            loop: 'true',
+            delay: 2000,
+            disableOnInteraction: 'true',
+        },
+    });
